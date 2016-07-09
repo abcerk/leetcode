4f29c3416e018dcdb1355ff3e05076b17d02117b
abcerk <abcerk@users.noreply.github.com>
2016-07-09 21:11:23 +0800

    [104. Maximum Depth of Binary Tree][Accepted]commited by abcerk

--- 104-Maximum-Depth-of-Binary-Tree/solution.java
+++ 104-Maximum-Depth-of-Binary-Tree/solution.java
@@ -9,21 +9,9 @@
  */
 public class Solution {
     public int maxDepth(TreeNode root) {
+        
         if(root == null) return 0;
-        int left = 0;
-        int right = 0;
-        if(root.left == null && root.right == null) {
-            return 1;
-        }
-        if(root.left != null) {
-            left += maxDepth(root.left);
-        }
-        
-        if(root.right != null) {
-            right += maxDepth(root.right);
-        }
-        
-        return Math.max(left, right)+1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
         
     }
 }
