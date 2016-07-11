7cdbfa7fdec7e68a7065c8fc8995909999c46582
abcerk <abcerk@users.noreply.github.com>
2016-07-11 21:58:08 +0800

    [373. Find K Pairs with Smallest Sums][Compile Error]commited by abcerk

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,25 +1,48 @@
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        if(nums1 == null || nums2 == null || k == 0) {
-            return null;
-        }
         
-        List<int[]> result = new ArrayList<>();
-        for(int i=0; i<nums1.length; i++) {
-            for(int j=0; j<nums2.length; j++) {
-                result.add(new int[]{nums1[i],nums2[j]});
-            }
-        }
-        
-       Collections.sort(result, new Comparator<int[]>(){
-            public int compare(int[] n1, int[] n2){
-                return (n1[0]+n1[1]) - (n2[0]+n2[1]);
-            }
-        });
-        
-        if(result.size()<k){
+        List<int[]> result = new ArrayList();
+        if(nums1 == null || nums2 == null || k == 0 || num1.length == 0 || num2.length == 0) {
             return result;
         }
-        return result.subList(0,k);
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>(k, new Comparator<Pair>() {
+           public int compare(Pair p1, Pair p2){
+                return p1.sum-p2.sum;
+            } 
+        });
+        
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                Pair p = new Pair(nums1[i], nums2[j]);
+                pq.offer(p);
+            }
+        }
+        
+        if(k>=pq.size()) k=pq.size();
+        
+        for(int i=0;i<k;i++){
+            Pair p = pq.poll();
+            result.add(new int[]{p.x, p.y});
+        }
+        return result;
+        
     }
+    
+    class Pair() {
+        int x;
+        int y;
+        int sum;
+        
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+            sum = x + y;
+        }
+    }
+        
+        
+        
+        
+        
 }
